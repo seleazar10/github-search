35c182acefc3701eb349be6460ec9b4cb8a48038
selea <sve5040@gmail.com>
2022-05-10 21:31:58 -0500

    able to render users - hard coded

---
+++ githubfinder/src/Testing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Testing extends Component {
+	render() {
+		return (
+			<div className="Testing">
+				<h2>Boom</h2>
+			</div>
+		);
+	}
+}
+
+export default Testing;
