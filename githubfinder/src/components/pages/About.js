31a77d9bc78e5f8fca33c189bc88bef87dda373c
selea <sve5040@gmail.com>
2022-07-10 15:57:25 -0500

    set up router

---
+++ githubfinder/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const About = () => {
+	return (
+		<div>
+			<h1>About this app</h1>
+			<p>App to search github user</p>
+		</div>
+	);
+};
+
+export default About;
